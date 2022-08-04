46b739b238f78da883f7c3175b70ce0c6151c6ff
Y.Emre USTUNER <webmasyer.yeu@gmail.com>
2022-08-04 09:30:22 +0300

    slm

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,31 +1,27 @@
 var routes = [
   {
-    path: "/",
-    url: "./index.html",
-    name: "index",
+    "path": "/",
+    "url": "./index.html",
+    "name": "index"
   },
   {
-    path: "/kayit",
-    url: "./kayit.html",
+    "path": "/kayit",
+    "url": "./kayit.html"
   },
   {
-    path: "/home",
-    url: "./home.html",
+    "path": "/home",
+    "url": "./home.html"
   },
   {
-    path: "/kayit",
-    url: "kayit.html",
+    "path": "/searchScreen",
+    "url": "./searchScreen.html"
   },
   {
-    path: "/searchScreen",
-    url: "./searchScreen.html",
+    "path": "/kayit",
+    "url": "kayit.html"
   },
   {
-    path: "/profil",
-    url: "./profil.html",
-  },
-  {
-    path: "/profil",
-    url: "/profil.html",
-  },
-];
+    "path": "/profil",
+    "url": "./profil.html"
+  }
+]
